d6a53d6e3aa788991c6962fb05bdf8be1056cae4
Matt <mgould@breakingfreegroup.com>
2020-03-27 18:49:57 +0000

    sudoku gui

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -1,6 +1,7 @@
 """ Runs the GUI """
 import pygame
 import copy
+import time
 
 pygame.init()
 pygame.font.init()
@@ -27,19 +28,21 @@ class Grid:
       [],[],[],[],[],[],[],[],[]
     ]
     self.selected = []
+    self.count = 0
 
     for i in range(9):
       for j in range(9):
-        self.cubes[i].append(Cube(width/9, height/9, i, j, self.board[i][j]))
+        self.cubes[i].append(Cube(width/9, height/9, i, j, self.board[j][i]))
 
   def draw(self):
+    print('drawing')
     squareWidth = (self.width / 9)
 
     pygame.draw.rect(self.screen, (255, 255, 255), (0, 0, 500, 500))
 
     for i in range(9):
       for j in range(9):
-        self.cubes[i][j].draw(self.screen)
+        self.cubes[i][j].draw(self.screen, self.board[j][i])
 
     for i in range(10):
       if i % 3 == 0 and i != 0:
@@ -70,23 +73,118 @@ class Grid:
       self.cubes[x][y].selected = True
       self.selected = [x, y]
 
+  def enter(self, num):
+    if self.selected[0] and self.selected[1]:
+      # self.cubes[self.selected[0]][self.selected[1]].value = num
+      self.board[self.selected[0]][self.selected[1]] = num
+
+
+  def getNextNode(self, posX, posY):
+    newX = 0
+    newY = 0
+
+    if posY < 8:
+      newX = posX
+      newY = posY + 1
+    elif posX < 8:
+      newX = posX + 1
+      newY = 0
+    else:
+      return False
+
+    if self.board[newX][newY] != 0:
+      return self.getNextNode(newX, newY)
+
+    return [newX, newY]
+
+  def solve(self, posX, posY):
+    for i in range(9):
+      self.board[posX][posY] = i + 1
+      
+      # self.draw(x, y)
+      # pygame.draw.rect(self.screen, (255, 255, 255), (0, 0, 500, 500))
+      self.cubes[posY][posX].draw(self.screen, i + 1)
+
+      if (self.isRowValid(posX) and self.isColValid(posY) and self.isCubeValid(posX, posY)):
+        nextNode = self.getNextNode(posX, posY)
+
+        if (nextNode == False):
+          return True
+
+        pygame.display.update()
+        pygame.time.delay(50)
+
+        if (self.solve(nextNode[0], nextNode[1])):
+          return True
+
+      self.board[posX][posY] = START[posX][posY] or 0
+
+  def isRowValid(self, posX):
+    for i in range(9):
+      if (self.board[posX].count(i + 1) > 1):
+        return False
+
+    return True
+
+  def isColValid(self, posY):
+    arr = []
+
+    for i in range(9):
+      arr.append(self.board[i][posY])
+
+    for j in range(9):
+      if arr.count(j + 1) > 1:
+        return False
+
+    return True
+
+  def isCubeValid(self, posX, posY):
+    yRange = False
+    xRange = False
+    arr = []
+
+    if posX < 3:
+      xRange = [0, 1, 2]
+    elif posX < 6:
+      xRange = [3, 4, 5]
+    else:
+      xRange = [6, 7, 8]
+
+    if posY < 3:
+      yRange = [0, 1, 2]
+    elif posY < 6:
+      yRange = [3, 4, 5]
+    else:
+      yRange = [6, 7, 8]
+
+    for x in xRange:
+      for y in yRange:
+        arr.append(self.board[x][y])
+
+    for i in range(9):
+      if arr.count(i + 1) > 1:
+        return False
+
+    return True
+
+
 class Cube:
   def __init__(self, width, height, row, col, value):
     self.selected = False
-    self.value = value
+    # self.value = value
     self.width = width
     self.height = height
     self.row = row
     self.col = col
 
-  def draw(self, screen):
+  def draw(self, screen, value):
     fnt = pygame.font.SysFont("comicsans", 40)
 
     if (self.selected):
       pygame.draw.rect(screen, (200, 200, 200), (int(self.row * self.width) + 1, int(self.col * self.height) + 1, int(self.width), int(self.height)))
     
-    text = str(self.value)
-    textSurface = fnt.render(text, 1, (128,128,128))
+    text = str(value) 
+    textSurface = fnt.render(text, 1, (128,128,128), (255,255,255))
     screen.blit(textSurface, (int(self.row * self.width) + int(self.width/2.7), int(self.col * self.height) + int(self.height/3.1)))
 
 def draw_window(board):
@@ -107,6 +205,29 @@ def main():
       if event.type == pygame.MOUSEBUTTONDOWN:
         pos = pygame.mouse.get_pos()
         x = grid.click(pos)
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_1:
+          grid.enter(0)
+        if event.key == pygame.K_1:
+          grid.enter(1)
+        if event.key == pygame.K_2:
+          grid.enter(2)
+        if event.key == pygame.K_3:
+          grid.enter(3)
+        if event.key == pygame.K_4:
+          grid.enter(4)
+        if event.key == pygame.K_5:
+          grid.enter(5)
+        if event.key == pygame.K_6:
+          grid.enter(6)
+        if event.key == pygame.K_7:
+          grid.enter(7)
+        if event.key == pygame.K_8:
+          grid.enter(8)
+        if event.key == pygame.K_9:
+          grid.enter(9)
+        if event.key == pygame.K_SPACE:
+          grid.solve(0, 0)
 
     draw_window(grid)
     pygame.display.update()

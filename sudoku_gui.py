5f9db5884f4e82ffb4f5cd8c0ef1eb1c5e2508de
Matt <mgould@breakingfreegroup.com>
2020-03-28 11:36:26 +0000

    sudoku gui

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -7,15 +7,15 @@ pygame.init()
 pygame.font.init()
 
 START = [
-  [0, 0, 0, 0, 0, 0, 2, 0, 0],
-  [0, 8, 0, 0, 0, 7, 0, 9, 0],
-  [6, 0, 2, 0, 0, 0, 5, 0, 0],
-  [0, 7, 0, 0, 6, 0, 0, 0, 0],
-  [0, 0, 0, 9, 0, 1, 0, 0, 0],
-  [0, 0, 0, 0, 2, 0, 0, 4, 0],
-  [0, 0, 5, 0, 0, 0, 6, 0, 3],
-  [0, 9, 0, 4, 0, 0, 0, 7, 0],
-  [0, 0, 6, 0, 0, 0, 0, 0, 0]
+  [4, 3, 9, 7, 1, 8, 0, 0, 0],
+  [0, 2, 0, 0, 0, 6, 3, 0, 0],
+  [0, 6, 0, 4, 0, 2, 0, 9, 0],
+  [1, 0, 0, 0, 9, 0, 4, 0, 0],
+  [0, 0, 8, 1, 0, 3, 5, 0, 0],
+  [0, 0, 5, 0, 7, 0, 0, 0, 3],
+  [0, 5, 0, 3, 0, 1, 0, 6, 0],
+  [0, 0, 4, 6, 0, 7, 0, 3, 0],
+  [0, 0, 0, 0, 0, 0, 0, 8, 0]
 ]
 
 class Grid:
@@ -29,13 +29,14 @@ class Grid:
     ]
     self.selected = []
     self.count = 0
+    self.solving = False
+    self.lineColour = (0,0,0)
 
     for i in range(9):
       for j in range(9):
         self.cubes[i].append(Cube(width/9, height/9, i, j, self.board[j][i]))
 
   def draw(self):
-    print('drawing')
     squareWidth = (self.width / 9)
 
     pygame.draw.rect(self.screen, (255, 255, 255), (0, 0, 500, 500))
@@ -50,10 +51,13 @@ class Grid:
       else:
         thickness = 1
 
-      pygame.draw.line(self.screen, (0, 0, 0), (0, int(i * squareWidth)), (self.height, int(i * squareWidth)), thickness)
-      pygame.draw.line(self.screen, (0, 0, 0), (int(i * squareWidth), 0), (int(i * squareWidth), self.width), thickness)
+      pygame.draw.line(self.screen, self.lineColour, (0, int(i * squareWidth)), (self.height, int(i * squareWidth)), thickness)
+      pygame.draw.line(self.screen, self.lineColour, (int(i * squareWidth), 0), (int(i * squareWidth), self.width), thickness)
 
   def click(self, pos):
+    if (self.solving):
+      return
+
     posX = pos[0]
     posY = pos[1]
 
@@ -74,9 +78,12 @@ class Grid:
       self.selected = [x, y]
 
   def enter(self, num):
-    if self.selected[0] and self.selected[1]:
+    if (self.solving):
+      return
+
+    if len(self.selected) > 0:
       # self.cubes[self.selected[0]][self.selected[1]].value = num
-      self.board[self.selected[0]][self.selected[1]] = num
+      self.board[self.selected[1]][self.selected[0]] = num
 
 
   def getNextNode(self, posX, posY):
@@ -97,12 +104,34 @@ class Grid:
 
     return [newX, newY]
 
+  def startSolve(self):
+    print('start')
+    self.solving = True
+    # Reset selected square if one exists
+    if len(self.selected) > 0:
+      self.cubes[self.selected[0]][self.selected[1]].selected = False
+      self.cubes[self.selected[0]][self.selected[1]].draw(self.screen, self.board[self.selected[1]][self.selected[0]])
+      self.cubes[self.selected[0]][self.selected[1]].clearBackground(self.screen)
+
+      self.selected = []
+      self.draw()
+
+    result = self.solve(0, 0)
+
+    if (result == True):
+      self.solving = False
+      self.lineColour = (0,255,0)
+    else:
+      self.solving = False
+      self.lineColour = (255,0,0)
+
   def solve(self, posX, posY):
     for i in range(9):
       self.board[posX][posY] = i + 1
       
-      # self.draw(x, y)
-      # pygame.draw.rect(self.screen, (255, 255, 255), (0, 0, 500, 500))
+      pygame.display.update()
+      pygame.time.delay(5)
+
       self.cubes[posY][posX].draw(self.screen, i + 1)
 
       if (self.isRowValid(posX) and self.isColValid(posY) and self.isCubeValid(posX, posY)):
@@ -110,9 +139,6 @@ class Grid:
 
         if (nextNode == False):
           return True
-
-        pygame.display.update()
-        pygame.time.delay(50)
 
         if (self.solve(nextNode[0], nextNode[1])):
           return True
@@ -176,22 +202,39 @@ class Cube:
     self.height = height
     self.row = row
     self.col = col
+    self.textColour = (128,128,128)
 
   def draw(self, screen, value):
     fnt = pygame.font.SysFont("comicsans", 40)
 
     if (self.selected):
       pygame.draw.rect(screen, (200, 200, 200), (int(self.row * self.width) + 1, int(self.col * self.height) + 1, int(self.width), int(self.height)))
+      textBackground = (200,200,200)
+    else:
+      # pygame.draw.rect(screen, (255, 255, 255), (int(self.row * self.width) + 1, int(self.col * self.height) + 1, int(self.width), int(self.height)))
+      textBackground = (255,255,255)
     
     text = str(value) 
-    textSurface = fnt.render(text, 1, (128,128,128), (255,255,255))
+    textSurface = fnt.render(text, 1, self.textColour, textBackground)
     screen.blit(textSurface, (int(self.row * self.width) + int(self.width/2.7), int(self.col * self.height) + int(self.height/3.1)))
 
-def draw_window(board):
+  def clearBackground(self, screen):
+    pygame.draw.rect(screen, (255, 255, 255), (int(self.row * self.width) + 1, int(self.col * self.height) + 1, int(self.width), int(self.height)))
+
+def draw_window(screen, board):
+  screen.fill((255,255,255))
+  fnt = pygame.font.SysFont("comicsans", 20)
+
+  text1 = fnt.render("Select a square to change the value", 1, (0,0,0))
+  text2 = fnt.render("Press space to solve", 1, (0,0,0))
+
+  screen.blit(text1, (138, 508))
+  screen.blit(text2, (180, 528))
+
   board.draw()
 
 def main():
-  screen = pygame.display.set_mode((500,500))
+  screen = pygame.display.set_mode((500,550))
   pygame.display.set_caption("Sudoku")
   grid = Grid(screen, 500, 500)
 
@@ -227,9 +270,9 @@ def main():
         if event.key == pygame.K_9:
           grid.enter(9)
         if event.key == pygame.K_SPACE:
-          grid.solve(0, 0)
+          grid.startSolve()
 
-    draw_window(grid)
+    draw_window(screen, grid)
     pygame.display.update()
 
 print('Running..')
